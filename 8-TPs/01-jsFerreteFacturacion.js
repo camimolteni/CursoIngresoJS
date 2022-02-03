8db627d4d6152567a94107764fd4886dfb2fe233
camimolteni <camilamolteni@gmail.com>
2022-02-03 01:29:08 -0300

    cambio

--- 8-TPs/01-jsFerreteFacturacion.js
+++ 8-TPs/01-jsFerreteFacturacion.js
@@ -1,17 +1,76 @@
-/*1.	Para el departamento de facturación:
+/*Camila Molteni DIV I
+TP 1
+1.	Para el departamento de facturación:
 A.	Ingresar tres precios de productos y mostrar la suma de los mismos.
 B.	Ingresar tres precios de productos y mostrar el promedio de los mismos.
 C.	ingresar tres precios de productos  y mostrar precio final (más IVA 21%).
 */
 function Sumar () 
 {
+	var precioUno;
+	var precioDos;
+	var precioTres;
+	var suma;
+
+	precioUno = txtIdPrecioUno.value;
+	precioDos = txtIdPrecioDos.value;
+	precioTres = txtIdPrecioTres.value;
+
+	precioUno = parseInt(precioUno);
+	precioDos = parseInt(precioDos);
+	precioTres = parseInt(precioTres);
+
+	suma = precioUno + precioDos + precioTres;
+
+	alert(suma);
 	
 }
 function Promedio () 
 {
+	var precioUno;
+	var precioDos;
+	var precioTres;
+	var suma;
+	var promedio;
+
+	precioUno = txtIdPrecioUno.value;
+	precioDos = txtIdPrecioDos.value;
+	precioTres = txtIdPrecioTres.value;
+
+	precioUno = parseInt(precioUno);
+	precioDos = parseInt(precioDos);
+	precioTres = parseInt(precioTres);
+
+	suma = precioUno + precioDos + precioTres;
+	promedio = suma / 3;
+
+	alert(promedio);
 	
 }
 function PrecioFinal () 
 {
+
+	var precioUno;
+	var precioDos;
+	var precioTres;
+	var suma;
+	var porcentaje;
+	var PrecioFinal;
+
+	precioUno = txtIdPrecioUno.value;
+	precioDos = txtIdPrecioDos.value;
+	precioTres = txtIdPrecioTres.value;
+
+	precioUno = parseInt(precioUno);
+	precioDos = parseInt(precioDos);
+	precioTres = parseInt(precioTres);
 	
+	suma = precioUno + precioDos + precioTres;
+	porcentaje = suma * 0.21; 
+	PrecioFinal = suma + porcentaje;
+
+	alert(PrecioFinal);
 }
+//txtIdPrecioUno
+//txtIdPrecioDos
+//txtIdPrecioTres 
--- 8-TPs/02-jsFerreteConstruccion.js
+++ 8-TPs/02-jsFerreteConstruccion.js
@@ -7,12 +7,49 @@ C.	Para hacer un contrapiso de 1m x 1m se necesitan 2 bolsas de cemento y 3 de c
 function Rectangulo () 
 {
 
+	var largo;
+	var ancho;
+	var perimetro;
+	var alambre;
+
+	largo = txtIdLargo.value;
+	ancho = txtIdAncho.value;
+
+	largo = parseInt(largo);
+	ancho = parseInt(ancho);
+	alambre = parseInt(alambre);
+
+	perimetro = (ancho + largo) * 2;
+	alambre = perimetro * 3;
+
+
+	alert(alambre);
+
 }
 function Circulo () 
 {
+
+	var radio;
+	var diametro;
+	var alambre;
+
+	radio = txtIdRadio.value;
+
+	radio = parseInt(radio);
+	diametro = parseInt(diametro);
+	alambre = parseInt(alambre);
+
+	radio = diametro / 2;
+	alambre = radio * 3;
+
+
+	alert(alambre);
 	
 }
 function Materiales () 
 {
 	
 }
+//txtIdLargo
+//txtIdAncho
+//txtIdRadio
