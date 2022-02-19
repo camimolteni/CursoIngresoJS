97346cd56742e97fc0a76cf4e94d690ee0b08f6f
camimolteni <camilamolteni@gmail.com>
2022-02-18 21:01:34 -0300

    cambio

--- 5-InstruccionWhile/jsIteracionesWhile-06.js
+++ 5-InstruccionWhile/jsIteracionesWhile-06.js
@@ -1,4 +1,10 @@
-function mostrar()
+/*Camila Molteni DIV I
+  WHILE 06
+
+  Al presionar el botón pedir 5 números e informar
+  la suma acumulada y el promedio.*/
+
+  function mostrar()
 {
 	var contador;
 	var acumulador;
@@ -7,7 +13,17 @@ function mostrar()
 	contador=0;
 	acumulador=0;
 	
+	while (contador < 5)
+	{
+		numeroIngresado = prompt("Ingrese un numero y presione aceptar. Hagalo 5 veces.");
+		numeroIngresado = parseInt(numeroIngresado);
+		acumulador += numeroIngresado;
+		contador ++; 
+	}
 	
-	txtIdSuma.value=acumulador;
-	txtIdPromedio.value=acumulador/5;
+	txtIdSuma.value = acumulador;
+	txtIdPromedio.value = acumulador / 5;
+
 }//FIN DE LA FUNCIÓN
+//txtIdSuma
+//txtIdPromedio
